7ed840b2a86a969d2a42f6161fcdb519bb8d1a1f
Tntmister <diogoamorim_2011@hotmail.com>
2026-06-15 19:31:10 +0100

    add sitemap structured data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,19 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<head>
+				<title>Fractal Block World Map</title>
+				<script type='application/ld+json'>
+					{JSON.stringify({
+						"@context": "https://schema.org",
+						"@type": "WebSite",
+						"name": "Fractal Block World Map",
+						"url": "https://tntmister.github.io/FractalBlockWorldMap",
+					})}
+				</script>
 				<meta
 					name='google-site-verification'
 					content='oyo0C9EsUZVa6qZaPylM7PInBZ_DK3jeMy_5I4towDk'
 				/>
-				<title>Fractal Block World Map</title>
 				<meta
 					name='description'
 					content='A (mostly) accurate map of Fractal Block World, including pathfinding from one place to another.'
